71cba000dbeb246eb57536bc32f0dc8fafff07a6
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-08 19:22:15 +0300

    add example7 Base

---
+++ example7_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 3;
+int b = 5;
+int c = 6;
+int d = 1;
+int e = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine(max);
